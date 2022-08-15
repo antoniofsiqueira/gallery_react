6cc0fcecbbb2509c791145934fdbf51564bc8c25
antoniofsiqueira <antonio.fsiqueira@gmail.com>
2022-08-15 19:01:36 -0300

    list dogs

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,13 +1,36 @@
+import React, { Component } from 'react';
+
 import logo from './logo.svg';
+import DogList from './DogList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-     <h2>Hellow Joe</h2>
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dogs: []
+    }
+  }
+
+  componentDidMount(){
+    //fetch("https://random.dog/woof.json")
+    fetch("https://dog.ceo/api/breeds/image/random/10")
+    .then((res)=> res.json())
+    .then((data)=>{
+      //console.log(data);
+      this.setState({dogs: data.url})
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">    
+     <h1 style={{textAlign: 'center'}}>Hellow Joe</h1>
+     <DogList dogs={this.state.dogs} />
     </div>
   );
+}
 }
 
 export default App;
---
+++ gallery/src/DogList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Dog from './Dog';
+import './DogsList.css'
+
+const DogList = (props) =>{
+    
+    const dogsArray = props.dogs.map((dogUrl)=>{
+        return <Dog url={dogUrl} />
+    })
+
+    return(
+        <div className='container'>
+            {dogsArray}
+        </div>
+    )
+}
+
+export default DogList;
