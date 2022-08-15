6dd4b9689dda142079ac8ecef440a9e44e25f819
antoniofsiqueira <antonio.fsiqueira@gmail.com>
2022-08-15 16:33:58 -0300

    initial commit

---
+++ gallery/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <h2>Hellow Joe</h2>
+    </div>
+  );
+}
+
+export default App;
